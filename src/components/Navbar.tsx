37e964945b72bf5081e733e714a9377077e0d714
Leon <leon.wehinger@htldornbirn.at>
2026-04-30 14:51:06 +0200

    feat(US-03): user logout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,37 +1,48 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+import { logoutUser } from "../services/auth";
 
 export function Navbar() {
   const { firebaseUser } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await logoutUser();
+    navigate("/login");
+  };
 
   return (
-    <header className="site-header">
-      <div className="container header-inner">
-        <Link className="logo" to="/">
-          Tournament Hub
-        </Link>
-        <nav aria-label="Primary Navigation">
-          <ul className="nav-list">
-            <li>
-              <NavLink to="/" end>
-                Home
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-        <div className="nav-auth">
-          {!firebaseUser && (
-            <>
-              <Link className="btn btn-ghost" to="/login">
-                Log in
-              </Link>
-              <Link className="btn btn-primary" to="/register">
-                Sign up
-              </Link>
-            </>
-          )}
+      <header className="site-header">
+        <div className="container header-inner">
+          <Link className="logo" to="/">
+            Tournament Hub
+          </Link>
+          <nav aria-label="Primary Navigation">
+            <ul className="nav-list">
+              <li>
+                <NavLink to="/" end>
+                  Home
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+          <div className="nav-auth">
+            {firebaseUser ? (
+                <button className="btn btn-ghost" onClick={handleLogout}>
+                  Log out
+                </button>
+            ) : (
+                <>
+                  <Link className="btn btn-ghost" to="/login">
+                    Log in
+                  </Link>
+                  <Link className="btn btn-primary" to="/register">
+                    Sign up
+                  </Link>
+                </>
+            )}
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
   );
 }
